757f8b1abc8564e20c57b189d9b3f0510b88542e
Jaaaniss <janis.liedeskalnins@valmierastehnikums.lv>
2023-01-23 14:17:58 +0200

    4.k

--- untitled.py
+++ untitled.py
@@ -6,6 +6,7 @@ data_json = json.loads(html.read())
 print(data_json)
 
 
-#json_object = json.dumps(data_json, indent=4)
-# with open("json/data.json", "w") as outfile:
-#     outfile.write(json_object)
+json_object = json.dumps(data_json, indent=4)
+with open("json/data.json", "w") as outfile:
+     outfile.write(json_object)
+
